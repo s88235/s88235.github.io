8c3dd91bbbf24487a15742c01350302b06f450ea
s88235 <s88235@informatik.htw-dresden.de>
2026-01-12 21:28:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {addCommands, start} from './annyang.js';
+//import {addCommands, start} from './annyang.js';
 
 //check if annyang could be loaded
 if(annyang) {
@@ -20,6 +20,7 @@ annyang.addCommands(commands);
 
 //start listening
 annyang.start();
+
 
 
 
