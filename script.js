51d67c76675c9060f6d67f303891a33c08819a71
s88235 <s88235@informatik.htw-dresden.de>
2026-01-12 16:36:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//check if annyang could be loaded
+if(annyang) {
+    console.log("annyang works")
+}
+
+//command Liste
+var commands = {
+    'Test' : greeting
+}
+
+//functions
+function greeting() {
+    console.log("Hello:3")
+}
+
+//add commands
+annynag.addCommands(commands);
+
+//start listening
+annyang.start();
+
